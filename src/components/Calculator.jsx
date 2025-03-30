9de72a2c4f89c05650afbad7fd2576bbfdad1814
zeritx <luisfabriziosarachaga@@gmail.com>
2025-03-30 21:46:00 +0200

    First commit by me and I start this proyect and promise finally this

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,45 @@
+import "./css/index.css"
+import "./css/normalize.css"
+function Calculator () {
+    return (
+        <>
+            <section className="Content-Calculator">
+                <section className="Calculator-Span">
+                    <span>‎</span>
+                </section>
+                <section className="Calculator-Buttons">
+                    <div className="Calculator_Inputs">
+                        <button>clr</button>
+                        <button>DEL</button>
+                        <button>%</button>
+                        <button>/</button>
+                    </div>
+                    <div className="Calculator_Inputs">
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button>*</button>
+                    </div>
+                    <div className="Calculator_Inputs">
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>-</button>
+                    </div>
+                    <div className="Calculator_Inputs">
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>+</button>
+                    </div>
+                    <div className="Calculator_Inputs">
+                        <button>0</button>
+                        <button>.</button>
+                        <button>=</button>
+                    </div>
+                </section>
+            </section>
+        </>
+    )
+}
+export default Calculator;
