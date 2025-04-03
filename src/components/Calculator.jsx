d861f481b9a0ee9bdb21f4907de6c80b86f160b2
zeritx <luisfabriziosarachaga@gmail.com>
2025-04-03 22:41:40 +0200

    Reset Laptop and finished the desing, only it need javascript code

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,41 +1,65 @@
 import "./css/index.css"
 import "./css/normalize.css" 
 function Calculator () {
+    let CompleteOperation = [];
+    let ResultOperation = "";
+    let NumberTemporary = "";
+    function GetNumber (e) {
+        let digit = e.target.innerText;
+        let Span = document.getElementById("Span");
+        NumberTemporary += digit;
+        Span.textContent = NumberTemporary;
+    }
+    function GiveOperator (e) {
+        let operator = e.target.innerText;
+        let Span = document.getElementById("Span");
+        Span.textContent = operator;
+        if (NumberTemporary !== "" ) {
+            CompleteOperation.push(NumberTemporary);
+            NumberTemporary = "";
+            console.log("Truth")
+        }
+    }
+    function ShowResult () {
+        let Span = document.getElementById("Span");
+        console.log(CompleteOperation);
+        Span.textContent = ResultOperation;
+    }
     return (
         <>
             <section className="Content-Calculator">
-                <section className="Calculator-Span">
-                    <span>‎0</span>
+                <section id="View" className="Calculator-Span">
+                    <span id="Span">0</span>
                 </section>
                 <section className="Calculator-Buttons">
                     <div className="Calculator_Inputs">
                         <button>clr</button>
                         <button>DEL</button>
                         <button>%</button>
-                        <button>/</button>
+                        <button onClick={GiveOperator}>/</button>
                     </div>
                     <div className="Calculator_Inputs">
-                        <button>7</button>
-                        <button>8</button>
-                        <button>9</button>
-                        <button>*</button>
+                        <button onClick={GetNumber}>7</button>
+                        <button onClick={GetNumber}>8</button>
+                        <button onClick={GetNumber}>9</button>
+                        <button onClick={GiveOperator}>*</button>
                     </div>
                     <div className="Calculator_Inputs">
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
-                        <button>-</button>
+                        <button onClick={GetNumber}>4</button>
+                        <button onClick={GetNumber}>5</button>
+                        <button onClick={GetNumber}>6</button>
+                        <button onClick={GiveOperator}>-</button>
                     </div>
                     <div className="Calculator_Inputs">
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>+</button>
+                        <button onClick={GetNumber}>1</button>
+                        <button onClick={GetNumber}>2</button>
+                        <button onClick={GetNumber}>3</button>
+                        <button onClick={GiveOperator}>+</button>
                     </div>
                     <div className="Calculator_Inputs">
-                        <button>0</button>
-                        <button>.</button>
-                        <button className="Button-Equal">=</button>
+                        <button onClick={GetNumber}>0</button>
+                        <button onClick={GiveOperator}>.</button>
+                        <button onClick={ShowResult} className="Button-Equal">=</button>
                     </div>
                 </section>
             </section>
