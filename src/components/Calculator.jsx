e48871fefdc283130b7343d4937beeaa516f3e5d
zeritx <luisfabriziosarachaga@gmail.com>
2025-04-12 11:48:47 +0200

    Realize the operators: +, -, *, / and %

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,29 +1,48 @@
-import "./css/index.css"
-import "./css/normalize.css" 
+import { StrictMode } from "react";
+import "./css/index.css";
+import "./css/normalize.css";
 function Calculator () {
-    let CompleteOperation = [];
-    let ResultOperation = "";
     let NumberTemporary = "";
+    let OperatorTemporary = "";
+    let ArrayNumbers = [];
+    let ArrayOperators = [];
     function GetNumber (e) {
         let digit = e.target.innerText;
-        let Span = document.getElementById("Span");
+        let SpanView = document.getElementById("Span");
         NumberTemporary += digit;
-        Span.textContent = NumberTemporary;
+        SpanView.innerText = NumberTemporary;
     }
-    function GiveOperator (e) {
+    function GetOperator (e) {
         let operator = e.target.innerText;
-        let Span = document.getElementById("Span");
-        Span.textContent = operator;
-        if (NumberTemporary !== "" ) {
-            CompleteOperation.push(NumberTemporary);
-            NumberTemporary = "";
-            console.log("Truth")
-        }
+        let SpanView = document.getElementById("Span");
+        OperatorTemporary = operator;
+        SpanView.innerText = OperatorTemporary;
+        ArrayNumbers.push(Number(NumberTemporary));
+        ArrayOperators.push(OperatorTemporary);
+        NumberTemporary = "";
     }
-    function ShowResult () {
-        let Span = document.getElementById("Span");
-        console.log(CompleteOperation);
-        Span.textContent = ResultOperation;
+    function ShowResult (e) {
+        let SpanView = document.getElementById("Span");
+        let ResultTemporary = ArrayNumbers[0];
+        ArrayNumbers.push(Number(NumberTemporary));
+        for (let i = 0; i < ArrayOperators.length; i++) {
+            let Number2 = ArrayNumbers[i + 1];
+            if (ArrayOperators[i] === "+") {
+                ResultTemporary += + Number2;
+            } else if (ArrayOperators[i] === "-") {
+                ResultTemporary -= Number2;
+            } else if (ArrayOperators[i] === "*") {
+                ResultTemporary *= Number2;
+            } else if (ArrayOperators[i] === "/") {
+                ResultTemporary /= Number2;
+            } else if (ArrayOperators[i] === "%") {
+                ResultTemporary = (ResultTemporary * Number2) / 100;
+            }
+            SpanView.innerText = ResultTemporary;
+        }
+        NumberTemporary = ResultTemporary;
+        ArrayNumbers = [];
+        ArrayOperators = [];
     }
     return (
         <>
@@ -35,30 +54,30 @@ function Calculator () {
                     <div className="Calculator_Inputs">
                         <button>clr</button>
                         <button>DEL</button>
-                        <button>%</button>
-                        <button onClick={GiveOperator}>/</button>
+                        <button onClick={GetOperator}>%</button>
+                        <button onClick={GetOperator}>/</button>
                     </div>
                     <div className="Calculator_Inputs">
                         <button onClick={GetNumber}>7</button>
                         <button onClick={GetNumber}>8</button>
                         <button onClick={GetNumber}>9</button>
-                        <button onClick={GiveOperator}>*</button>
+                        <button onClick={GetOperator}>*</button>
                     </div>
                     <div className="Calculator_Inputs">
                         <button onClick={GetNumber}>4</button>
                         <button onClick={GetNumber}>5</button>
                         <button onClick={GetNumber}>6</button>
-                        <button onClick={GiveOperator}>-</button>
+                        <button onClick={GetOperator}>-</button>
                     </div>
                     <div className="Calculator_Inputs">
                         <button onClick={GetNumber}>1</button>
                         <button onClick={GetNumber}>2</button>
                         <button onClick={GetNumber}>3</button>
-                        <button onClick={GiveOperator}>+</button>
+                        <button onClick={GetOperator}>+</button>
                     </div>
                     <div className="Calculator_Inputs">
                         <button onClick={GetNumber}>0</button>
-                        <button onClick={GiveOperator}>.</button>
+                        <button onClick={GetOperator}>.</button>
                         <button onClick={ShowResult} className="Button-Equal">=</button>
                     </div>
                 </section>
